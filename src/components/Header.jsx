9137760152284f2524cb628a2f567a7b102398ab
RomanBashtovyiDev <96292153+RomanBashtovyiDev@users.noreply.github.com>
2022-02-03 12:10:34 +0300

    gh-pages added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,18 @@ function Header() {
     <>
       <nav className="cyan lighten-4">
         <div className="nav-wrapper">
-          <a href="#" className="brand-logo">
+          <a href="/" className="brand-logo">
             FortniteShop
           </a>
           <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li>
-              <a href="!#">repo</a>
+              <a
+                href="https://github.com/Romanuss-it/fortniteApi-shop-react-"
+                target="_blank"
+                rel="noreferrer"
+              >
+                repo
+              </a>
             </li>
           </ul>
         </div>
